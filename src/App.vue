dbbf870e761ff8d59a8614991b64c08368e66b49
“李强” <1411183501@qq.com>
2021-05-12 14:25:24 +0800

    修改组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
   <div id="app">
    <h1>App title</h1>
+   <about></about>
+   <home></home>
   </div>
 </template>
+
+<script>
+
+import about from './components/About'
+import home from './views/Home'
+
+export default {
+  name:'app',
+  components:{
+    about,
+    home  
+  }
+}
+
+    Home
+    Home</script>
 
 <style>
 
---
+++ src/components/About.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="about">
+        <h5>this is about</h5>
+    </div>
+</template>
